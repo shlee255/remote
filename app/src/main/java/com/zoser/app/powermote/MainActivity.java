a680bd779dbe858fe0f151cff6dbf65a5e2456e1
Hong <shlee255@gmail.com>
2022-04-21 23:16:54 +0800

    Update layout

--- app/src/main/java/com/zoser/app/powermote/MainActivity.java
+++ app/src/main/java/com/zoser/app/powermote/MainActivity.java
@@ -20,10 +20,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
     private ImageButton _buttonPowerAll;
 
-    private LinearLayout [] _button = new LinearLayout[12];
-    private IRMessageRequest [] _buttonRequest = new IRMessageRequest[12];
+    private LinearLayout [] _button = new LinearLayout[32];
+    private IRMessageRequest [] _buttonRequest = new IRMessageRequest[32];
 
-    private LinearLayout [] _rows = new LinearLayout[4];
+    private LinearLayout [] _rows = new LinearLayout[8];
 
     private View _currentClickedView = null;
 
@@ -54,6 +54,45 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
 
         _buttonPowerAll = (ImageButton) findViewById(R.id.id_Button_ALL);
 
+        _button[0] =   createIRButton("1",R.drawable.icon_01,_rows[0], new IRMessageRequest(IRMessages.KONKA_01));
+        _button[1] =   createIRButton("2",R.drawable.icon_01,_rows[0],new IRMessageRequest(IRMessages.KONKA_02));
+        _button[2] =   createIRButton("3",R.drawable.icon_01,_rows[0],new IRMessageRequest(IRMessages.KONKA_03));
+        _button[3] =   createIRButton("4",R.drawable.icon_01,_rows[0], new IRMessageRequest(IRMessages.KONKA_04));
+        _button[4] =   createIRButton("5",R.drawable.icon_01,_rows[1],new IRMessageRequest(IRMessages.KONKA_05));
+        _button[5] =   createIRButton("6",R.drawable.icon_01,_rows[1],new IRMessageRequest(IRMessages.KONKA_06));
+        _button[6] =   createIRButton("7",R.drawable.icon_01,_rows[1], new IRMessageRequest(IRMessages.KONKA_07));
+        _button[7] =   createIRButton("8",R.drawable.icon_01,_rows[1],new IRMessageRequest(IRMessages.KONKA_08));
+        _button[8] =   createIRButton("TV",R.drawable.icon_03,_rows[2],new IRMessageRequest(IRMessages.KONKA_TV));
+        _button[9] =   createIRButton("9",R.drawable.icon_01,_rows[2], new IRMessageRequest(IRMessages.KONKA_09));
+        _button[10] =  createIRButton("0",R.drawable.icon_01,_rows[2],new IRMessageRequest(IRMessages.KONKA_00));
+        _button[11] =  createIRButton("HTMI",R.drawable.icon_03,_rows[2],new IRMessageRequest(IRMessages.KONKA_HDMI));
+
+        _button[12] =  createIRButton("EPS",R.drawable.icon_04,_rows[3], new IRMessageRequest(IRMessages.KONKA_EPS));
+        _button[13] =  createIRButton("^",R.drawable.icon_01,_rows[3],new IRMessageRequest(IRMessages.KONKA_UP));
+        _button[14] =  createIRButton("Menu",R.drawable.icon_04,_rows[3], new IRMessageRequest(IRMessages.KONKA_MENU));
+        _button[15] =  createIRButton("Audio",R.drawable.icon_01,_rows[3],new IRMessageRequest(IRMessages.KONKA_AUDIO));
+
+        _button[16] =  createIRButton("<",R.drawable.icon_02,_rows[4],new IRMessageRequest(IRMessages.KONKA_LEFT));
+        _button[17] =  createIRButton("OK",R.drawable.icon_04,_rows[4], new IRMessageRequest(IRMessages.KONKA_OK));
+        _button[18] =  createIRButton(">",R.drawable.icon_01,_rows[4],new IRMessageRequest(IRMessages.KONKA_RIGHT));
+        _button[19] =  createIRButton("Exit",R.drawable.icon_02,_rows[4],new IRMessageRequest(IRMessages.KONKA_EXIT));
+
+        _button[20] =  createIRButton("VOLUME",R.drawable.icon_02,_rows[5], new IRMessageRequest(IRMessages.KONKA_VOLUME_UP));
+        _button[21] =  createIRButton("DOWN",R.drawable.icon_01,_rows[5],new IRMessageRequest(IRMessages.KONKA_DOWN));
+        _button[22] =  createIRButton("INFO",R.drawable.icon_03,_rows[5],new IRMessageRequest(IRMessages.KONKA_INFO));
+        _button[23] =  createIRButton("CHANNEL",R.drawable.icon_02,_rows[5], new IRMessageRequest(IRMessages.KONKA_CHANNEL_UP));
+
+        _button[24] =  createIRButton("VOLUME",R.drawable.icon_08,_rows[6],new IRMessageRequest(IRMessages.KONKA_VOLUME_DOWN));
+        _button[25] =  createIRButton("",R.drawable.icon_03,_rows[6],new IRMessageRequest(IRMessages.KONKA_02));
+        _button[26] =  createIRButton("",R.drawable.icon_04,_rows[6],new IRMessageRequest(IRMessages.KONKA_00));
+        _button[27] =  createIRButton("CHANNEL",R.drawable.icon_08,_rows[6],new IRMessageRequest(IRMessages.KONKA_CHANNEL_DOWN));
+
+        _button[28] =  createIRButton("RED",   R.drawable.icon_04,_rows[7],new IRMessageRequest(IRMessages.KONKA_01));
+        _button[29] =  createIRButton("GREEN", R.drawable.icon_04,_rows[7],new IRMessageRequest(IRMessages.KONKA_02));
+        _button[30] =  createIRButton("YELLOW",R.drawable.icon_04,_rows[7],new IRMessageRequest(IRMessages.KONKA_00));
+        _button[31] =  createIRButton("BLUE",  R.drawable.icon_04,_rows[7],new IRMessageRequest(IRMessages.KONKA_BLUE));
+
+/*
         _button[0] =  createIRButton("HDMI HUB",R.drawable.icon_04,_rows[0], new IRMessageRequest(IRMessages.KONKA_00));
         _button[1] =  createIRButton("LG TV",R.drawable.icon_01,_rows[0],new IRMessageRequest(IRMessages.KONKA_01));
         _button[2] =  createIRButton("HOME THEATER",R.drawable.icon_03,_rows[0],new IRMessageRequest(IRMessages.KONKA_02));
@@ -69,10 +108,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         _button[9] =  createIRButton("VOLUME DOWN",R.drawable.icon_08,_rows[3],new IRMessageRequest(IRMessages.KONKA_09));
         _button[10] =  createIRButton("SWITCH",R.drawable.icon_07,_rows[3],new IRMessageRequest(IRMessages.KONKA_10));
         _button[11] =  createIRButton("VOLUME DOWN",R.drawable.icon_08,_rows[3],new IRMessageRequest(IRMessages.KONKA_11));
-
+*/
         _lastBurstTime = System.nanoTime();
 
-        for(int a=0;a<12;a++)
+        for(int a=0;a<28;a++)
         {
             _button[a].setOnTouchListener(this);
         }
@@ -110,7 +149,10 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
         _rows[1] = (LinearLayout)findViewById(R.id.id_Layout_Row_02);
         _rows[2] = (LinearLayout)findViewById(R.id.id_Layout_Row_03);
         _rows[3] = (LinearLayout)findViewById(R.id.id_Layout_Row_04);
-
+        _rows[4] = (LinearLayout)findViewById(R.id.id_Layout_Row_05);
+        _rows[5] = (LinearLayout)findViewById(R.id.id_Layout_Row_06);
+        _rows[6] = (LinearLayout)findViewById(R.id.id_Layout_Row_07);
+        _rows[7] = (LinearLayout)findViewById(R.id.id_Layout_Row_08);
     }
 
 
